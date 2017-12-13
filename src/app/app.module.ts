da51ce090220d45f59a57b08bc847487804c6d46
GoutamiThatipalli <goutami.thatipalli@imaginea.com>
2017-12-13 12:37:05 +0530

    updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { CategoryComponent } from './components/category/category.component';
+import { EventsComponent } from './components/category/events/events.component';
 
+
+const appRoutes: Routes = [
+  { path: 'categories', component: CategoryComponent },
+  { path: 'events', component: EventsComponent },
+  {
+    path: 'categories',
+    component: CategoryComponent,
+    data: { title: 'Categories' }
+  },
+  { path: '',
+    redirectTo: '/categories',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CategoryComponent,
+    EventsComponent
   ],
-  imports: [
+  imports: [ RouterModule.forRoot(
+    appRoutes,
+    { enableTracing: true } 
+  ),
     BrowserModule
   ],
   providers: [],
